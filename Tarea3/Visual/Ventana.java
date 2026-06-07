0704a6187d9351fbcaa8d4cc4f04f967a16c2c84
Maximiliano Pinto <mpinto2025@udec.cl>
2026-06-07 19:48:42 -0400

    feat: crear Clase Ventana

---
+++ Tarea3/Visual/Ventana.java
@@ -0,0 +1,22 @@
+package Visual;
+import javax.swing.*;
+import java.awt.*;
+
+public class Ventana extends JFrame {
+    public Ventana() {
+        setTitle("Expendedor");
+        setSize(900, 650);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        JPanelPrincipal panelPrincipal = new JPanelPrincipal();
+        add(panelPrincipal, BorderLayout.CENTER);
+
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Ventana());
+    }
+}
