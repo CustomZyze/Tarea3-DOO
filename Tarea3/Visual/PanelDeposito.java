4c9a4661f39ba22053b7b96150dcb679729a7f00
Maximiliano Pinto <mpinto2025@udec.cl>
2026-06-08 00:51:48 -0400

    feat: iniciar panel de producto

--- Tarea3/Visual/PanelDeposito.java
+++ Tarea3/Visual/PanelDeposito.java
@@ -1,4 +1,30 @@
 package Visual;
+import Logica.Deposito;
+import Logica.Producto;
+import Logica.Moneda;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class PanelDeposito {
-}
+    private int x, y;
+    private static final int ANCHO = 75, ALTO = 340;
+    private Deposito<?> deposito;
+    private String etiqueta;
+    private Color color;
+    private ArrayList<PanelProducto> vistasProductos = new ArrayList<>();
+    private ArrayList<PanelMoneda>   vistasMonedas   = new ArrayList<>();
+    private boolean esMonedas;
+
+    public PanelDeposito(int x, int y, Deposito<?> dep, String etiqueta, Color color) {
+        this.x        = x;
+        this.y        = y;
+        this.deposito = dep;
+        this.etiqueta = etiqueta;
+        this.color    = color;
+
+        // detectar si el depósito es de monedas o productos
+        // según la etiqueta (puedes cambiarlo si prefieres otro criterio)
+        this.esMonedas = etiqueta.equals("Vuelto");
+
+        actualizar();
+    }
