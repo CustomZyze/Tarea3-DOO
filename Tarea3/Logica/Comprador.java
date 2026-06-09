307b6aa753b5d056969254047dfa403a898610c0
Matias Cabello <maticab170@gmail.com>
2026-06-09 17:17:30 -0400

    feat: Actualizar logica base y estructura de constructor en Comprador

--- Tarea3/Logica/Comprador.java
+++ Tarea3/Logica/Comprador.java
@@ -1,5 +1,7 @@
 package Logica;
 
+import java.util.ArrayList;
+
 /**
  * Clase que representa a un comprador dentro del sistema.
  * El comprador intenta comprar un producto en el expendedor utilizando una moneda.
@@ -11,46 +13,12 @@ public class Comprador {
      * Resultado de consumir el producto comprado.
      */
     private String sabor;
+    private ArrayList<Moneda> monedero;
 
-    /**
-     * Suma total del vuelto recibido por el comprador.
-     */
-    private int vuelto;
 
-    /**
-     * Constructor del comprador el cual realiza la compra de un producto en el expendedor usando una moneda.
-     * Si recibe un producto válido, lo consume y guarda el resultado.
-     * Luego obtiene todas las monedas de vuelto disponibles y suma sus valores.
-     * @param m moneda utilizada para realizar la compra.
-     * @param cualProducto producto que se desea comprar, indicado mediante una enumeración.
-     * @param exp expendedor donde se realiza la compra.
-     * @throws PagoIncorrectoException si no se ingresa una moneda válida.
-     * @throws NoHayProductoException si el producto solicitado no existe o no queda stock.
-     * @throws PagoInsuficienteException si el valor de la moneda no alcanza para comprar el producto.
-     */
-    public Comprador(Moneda m, Enumeracion cualProducto, Expendedor exp) throws PagoIncorrectoException, NoHayProductoException, PagoInsuficienteException{
-        this.vuelto = 0;
-        try {
-            exp.comprarProducto(m , cualProducto);
-            Producto p = exp.getProducto();
-
-            if( p != null){
-                this.sabor = p.consumir();
-            }
-        } finally {
-            Moneda v;
-            while((v = exp.getVuelto()) != null){
-                this.vuelto += v.getValor();
-            }
-        }
-    }
-
-    /**
-     * Retorna el total del vuelto recibido por el comprador.
-     * @return suma total del vuelto recibido.
-     */
-    public int cuantoVuelto(){
-        return vuelto;
+    public Comprador() throws PagoIncorrectoException, NoHayProductoException, PagoInsuficienteException{
+        this.monedero = new ArrayList<>();
+        this.sabor = null;
     }
 
     /**
